5de8138e322436328f903d2c65843f932027144e
Maceo <basse.maceo@gmail.com>
2024-03-20 10:29:49 +0100

    Ajouter la liste de tâches à l'application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
-
-
   return (
     <>
-     
+      <div>
+        <h1>Todo List</h1>
+        <TodoList />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
