b952c2f40202522ea1a97c31a7418de8932f60f7
brice.matias.e <brice.matias.e@thalesdigital.io>
2024-04-08 10:18:27 +0200

    test line coverage

--- backend/bin/console.ts
+++ backend/bin/console.ts
@@ -49,3 +49,4 @@ new Ignitor(APP_ROOT, { importer: IMPORTER })
     process.exitCode = 1
     prettyPrintError(error)
   })
+// NOSONAR
--- backend/bin/server.ts
+++ backend/bin/server.ts
@@ -45,3 +45,4 @@ new Ignitor(APP_ROOT, { importer: IMPORTER })
     process.exitCode = 1
     prettyPrintError(error)
   })
+// NOSONAR
